83fd7bcb2bab5bb81d7f76f9a39d19d4fd72debc
Loppes Filho <loppes@megganet.com.br>
2021-06-19 17:39:55 -0300

    DTO, Repositories and seed

---
+++ src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
@@ -0,0 +1,49 @@
+package com.devsuperior.movieflix.dto;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.Movie;
+
+public class GenreDTO {
+
+	private Long id;
+	private String name;
+	private Set<Movie> movies = new HashSet<>();
+	
+	public GenreDTO() {
+	}
+
+	public GenreDTO(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	public GenreDTO(Genre entity) {
+		id = entity.getId();
+		name = entity.getName();
+		entity.getMovies().forEach(movie -> new MovieDTO(movie));
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Movie> getMovies() {
+		return movies;
+	}	
+}
